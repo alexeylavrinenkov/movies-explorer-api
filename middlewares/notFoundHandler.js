7188e5686042da1a4d62eebbe7928896ce97fcfe
alexeylavrinenkov <lavrinenkovalexey@yandex.ru>
2023-02-28 04:39:54 +0500

    refactor: вынести сообщение ошибки в константы

--- middlewares/notFoundHandler.js
+++ middlewares/notFoundHandler.js
@@ -1,5 +1,6 @@
 const NotFoundError = require('../errors/NotFoundError');
+const { ERROR_MESSAGES } = require('../utils/constants');
 
 module.exports = (req, res, next) => {
-  next(new NotFoundError('Ресурс не найден'));
+  next(new NotFoundError(ERROR_MESSAGES.PAGE_NOT_FOUND));
 };
