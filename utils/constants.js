3113b250c54eb60506021858f58c8091e86153ff
alexeylavrinenkov <lavrinenkovalexey@yandex.ru>
2023-02-28 04:45:22 +0500

    fix: исправить адрес сайта

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,11 @@
+const DEFAULT_CONFIG = {
+  PORT: 3000,
+  DB_URL: 'mongodb://127.0.0.1:27017/bitfilmsdb',
+};
+
 const DEV_SECRET_KEY = 'dev-secret-key';
+
+const LINK_REGEX = /^https?:\/\/(www\.)?[0-9a-zA-Z]+([.|-]{1}[0-9a-zA-Z]+)*\.[0-9a-zA-Z-]+(\/[0-9a-zA-Z\-._~:/?#[\]@!$&'()*+,;=]*#?)?$/;
 
 const ERROR_MESSAGES = {
   INCORRECT_LOGIN_OR_PASSWORD: 'Вы ввели неправильный логин или пароль.',
@@ -17,17 +24,19 @@ const ERROR_MESSAGES = {
 };
 
 const ALLOWED_CORS = [
-  'https://',
-  'https://',
-  'http://',
-  'http://',
+  'https://movies-explorer.service.nomoredomains.work',
+  'https://api.movies-explorer.site.nomoredomains.work',
+  'http://movies-explorer.service.nomoredomains.work',
+  'http://api.movies-explorer.site.nomoredomains.work',
   'localhost:3000',
 ];
 
 const ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 module.exports = {
+  DEFAULT_CONFIG,
   DEV_SECRET_KEY,
+  LINK_REGEX,
   ERROR_MESSAGES,
   ALLOWED_CORS,
   ALLOWED_METHODS,
